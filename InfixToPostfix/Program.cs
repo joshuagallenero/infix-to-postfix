0a6c6306471622bad40a93d229658279516fe760
joshuagallenero <mjjpgallenero@gmail.com>
2019-09-23 15:06:42 +0800

    Initial commit

---
+++ InfixToPostfix/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfixToPostfix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var groupingcheck = new GroupingCheck();
+            
+            Console.WriteLine("Please enter an expression.");
+            string infix = Console.ReadLine();
+            if (!groupingcheck.IsValidGrouping(infix))
+            {
+                Console.WriteLine("Brackets for grouping are incorrect. Please enter a valid expression.");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Please enter an expression.");
+                infix = Console.ReadLine();
+            }
+
+            var InfixToPostfixConverter = new InfixToPostfixConverter(infix);
+            Console.WriteLine(InfixToPostfixConverter.Output);
+            Console.ReadLine();
+        }
+    }
+}
